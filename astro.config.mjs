0a363a919db5c92c25c38e43dbd97e1b675534db
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-05-23 12:03:14 +0300

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import mdx from "@astrojs/mdx";
+import astroExpressiveCode from "astro-expressive-code";
+import react from "@astrojs/react";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    solid({
+      include: ["**/solid/*"],
+    }),
+    react({
+      include: ["**/react/*"],
+    }),
+    astroExpressiveCode(),
+    mdx(),
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
